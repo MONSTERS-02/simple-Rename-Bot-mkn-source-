0a9e0eedf7fe093d93497cce75ecfa0281eb1a49
MONSTERS-02 <145355316+MONSTERS-02@users.noreply.github.com>
2023-10-05 22:23:50 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "28645930"))
+API_HASH = environ.get("API_HASH", "a123ec5ed26912060fe08ccd6e7d0779")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6682036618:AAHXsKTT43mai5nL0k-XPm1UVXutjrZL0Cs")
+ADMIN = int(environ.get("ADMIN", "6257065375"))          
+CAPTION = environ.get("CAPTION", "@ANIMESAMA_VOSTFR")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://graph.org/file/79d1c9b8e2a63e346b2e0.jpg")
